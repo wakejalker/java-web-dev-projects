0f317033b07853c10a0d16de44ec2a698fcd2ad2
jake <32205542+wakejalker@users.noreply.github.com>
2024-04-15 15:27:28 -0500

    Chapter 6 Exercises Complete

--- unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
@@ -1,11 +1,42 @@
 package org.launchcode;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
+
+    Car test_car;
+
+    @BeforeEach
+    public void createCarObject() {
+        test_car = new Car ("Toyota", "Prius", 10, 50);
+    }
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+    @Test
+    public void emptyTest() {
+        assertEquals(10,10,.001);
+    }
     //TODO: constructor sets gasTankLevel properly
+//    @Test
+//    public void testInitialGasTank() {
+//        assertEquals(10, test_car.getGasTankLevel(), .001);
+//    }
+//
+    // Write another version of testInitialGasTank() using assertFalse(), comparing the value to 0.
+//    @Test
+//    public void testInitialGasTank() {
+//        assertFalse(test_car.getGasTankLevel() == 0);
+//    }
+
+    // Write another version of testInitialGasTank() using assertTrue().
+    @Test
+    public void testInitialGasTank() {
+        assertTrue(test_car.getGasTankLevel() == 10);
+    }
     //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
 }
