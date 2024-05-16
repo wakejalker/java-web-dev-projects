48634a93b11f4a068da3b88c49313e7c6dfd1a39
jake <32205542+wakejalker@users.noreply.github.com>
2024-05-16 00:54:35 -0500

    Chapter 6 Exercises

--- unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
@@ -2,41 +2,48 @@ package org.launchcode;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-
     Car test_car;
 
     @BeforeEach
-    public void createCarObject() {
-        test_car = new Car ("Toyota", "Prius", 10, 50);
+    public void initCar() {
+        test_car = new Car("Toyota", "Prius", 10, 50);
     }
-    //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+
+    //TODO: add emptyTest so we can configure our runtime environment
     @Test
     public void emptyTest() {
-        assertEquals(10,10,.001);
+        assertEquals(10, 10, .001);
     }
-    //TODO: constructor sets gasTankLevel properly
-//    @Test
-//    public void testInitialGasTank() {
-//        assertEquals(10, test_car.getGasTankLevel(), .001);
-//    }
-//
-    // Write another version of testInitialGasTank() using assertFalse(), comparing the value to 0.
-//    @Test
-//    public void testInitialGasTank() {
-//        assertFalse(test_car.getGasTankLevel() == 0);
-//    }
 
-    // Write another version of testInitialGasTank() using assertTrue().
+    //TODO: constructor sets gasTankLevel properly
     @Test
     public void testInitialGasTank() {
-        assertTrue(test_car.getGasTankLevel() == 10);
+        assertEquals( 10, test_car.getGasTankLevel(),.001);
     }
+
     //TODO: gasTankLevel is accurate after driving within tank range
     @Test
-
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+//        double expectedGasTankLevel = 9;
+//        assertEquals(expectedGasTankLevel, test_car.getGasTankLevel(), .001);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    @Test
+    public void testGasTankAfterExceedingTankRange() {
+        test_car.drive(501);
+        assertEquals(test_car.getGasTankLevel(), 0, .001);
+    }
+
     //TODO: can't have more gas than tank size, expect an exception
+    @Test
+    public void testGasOverfillException() {
+        assertThrows(IllegalArgumentException.class, () -> test_car.addGas(5), "Shouldn't get here, car cannot have more gas in tank than the size of the tank.");
+
+    }
 }
